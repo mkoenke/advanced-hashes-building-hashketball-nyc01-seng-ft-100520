44d7272ba3c2d801e073b14a9b9f54543221d668
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-21 15:50:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
   {
     home: {
       team_name: "Brooklyn Nets",
-      team_colors: ["Black", "White"],
+      colors: ["Black", "White"],
       players: [
         {:player_name => "Alan Anderson",
         :number => 0,
@@ -61,7 +61,7 @@ def game_hash
     },
     away: {
       team_name: "Charlotte Hornets",
-      team_colors: ["Turquoise", "Purple"],
+      colors: ["Turquoise", "Purple"],
       players: [
         {:player_name => "Jeff Adrien",
         :number => 4,
